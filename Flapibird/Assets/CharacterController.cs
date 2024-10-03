46ba125ed7721c95f1c7a5211ccaf17061c1fcba
Peluchomp <pau.mora.sole@gmail.com>
2024-10-03 10:59:56 +0200

    FlappyBird

---
+++ Flapibird/Assets/CharacterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Rigidbody2D rigidBody;
+    [SerializeField] float jumpForce = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+       } 
+    }
+}
